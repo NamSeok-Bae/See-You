a590d736153df846cb905e609072b6d324e7fe00
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2024-05-03 12:48:10 +0900

    [add]: 로그인 뷰컨트롤러 생성

---
+++ SeeYou/SeeYou/Presentation/Auth/ViewController/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  SeeYou
+//
+//  Created by 배남석 on 2024/05/03.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Helpers
+}
