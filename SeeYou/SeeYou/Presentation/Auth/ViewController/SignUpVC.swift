41b49d22e6a218346b4b6db4d592cdd398e03801
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2024-05-03 12:48:20 +0900

    [add]: 회원가입 VC 생성

---
+++ SeeYou/SeeYou/Presentation/Auth/ViewController/SignUpVC.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpVC.swift
+//  SeeYou
+//
+//  Created by 배남석 on 2024/05/03.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Helpers
+
+}
