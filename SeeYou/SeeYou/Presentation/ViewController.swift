17e33d4f0a45772dcae7fca76f5cf948f29ed941
배남석 <76683388+NamSeok-Bae@users.noreply.github.com>
2024-05-03 12:44:24 +0900

    [change]: 스토리보드 삭제 및 코드베이스 환경으로 변경.

---
+++ SeeYou/SeeYou/Presentation/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SeeYou
+//
+//  Created by 배남석 on 2024/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - UI properties
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Helpers
+}
+
